98422bfd28d87e9c2042743b8826b21e3ad8d5c1
ItsMIkey936 <miguelarellan117@gmail.com>
2026-05-07 22:10:38 -0600

    feat: Add chat and trends page

---
+++ app/chat/page.tsx
@@ -0,0 +1,5 @@
+export default function Chat() {
+  return (
+    <h1>Muy buenas, aqui esta el chatbot</h1>
+  )
+}
---
+++ app/trends/page.tsx
@@ -0,0 +1,5 @@
+export default function Trends() {
+  return (
+    <h1>Aqui van los trends</h1>
+  )
+}
