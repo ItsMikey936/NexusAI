17ede17c6af773043184ad87d99b49c3691a1e9e
ItsMikey936 <miguelarellan117@gmail.com>
2026-05-12 22:01:58 -0600

    test: Fix navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,6 @@ export default function Navbar() {
       <div className="flex gap-6">
         <Link href="/" className="text-gray-600 hover:text-blue-600">Inicio</Link>
         <Link href="/chat" className="text-gray-600 hover:text-blue-600">Chatbot</Link>
-        <Link href="/trends" className="text-gray-600 hover:text-blue-600">Tendencias</Link>
       </div>
     </nav>
   )
